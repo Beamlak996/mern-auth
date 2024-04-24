fcbc9a80cdb1cd150ec9cb3e71888b15c9a2d85a
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-04-24 12:16:54 -0900

    connect to db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,21 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+mongoose
+  .connect(process.env.MONGO_URL!)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Mongo Db error", error);
+  });
 
-app.get('/', (req: Request, res: Response)=> {
-    res.send("hello world")
-})
+const app = express();
 
-app.listen(5000)
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
+
+app.listen(5000);

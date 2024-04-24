0f668c573b4fa8223167d1999e2ea8fffc1a7304
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-04-24 11:37:05 -0900

    shadcn setup done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <div className='text-3xl text-rose-500' >
       Hello There
+      <Button>Click Here</Button>
     </div>
   )
 }

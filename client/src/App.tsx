308575ae9b57e31e3021483fefd213c864bda8d1
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-04-25 23:00:27 -0900

    update header component and make profile private

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { About } from "./pages/About"
-import { Profile } from "./pages/Profile"
-import { SignIn } from "./pages/SignIn"
-import { SignUp } from "./pages/SignUp"
-import { Header } from "./components/header"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Profile } from "./pages/Profile";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Header } from "./components/header";
+import { PrivateRoute } from "./components/private-route";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Header />
@@ -16,10 +16,12 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux"
 
 export const Header = () => {
+  const { currentUser } = useSelector((state: any)=>state.user)
+
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,8 +17,15 @@ export const Header = () => {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/sign-in">
+          <Link to="/profile">
+            {currentUser ? (
+              <div className="h-7 w-7 rounded-full bg-blue-300 flex items-center justify-center text-white" >
+                  PP
+              </div>
+            ) : (
+
             <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
